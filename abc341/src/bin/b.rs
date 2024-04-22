899d3934c4aa8f58a09ddd3dd85c22edbd461fc5
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-22 20:52:37 +0900

    b

--- abc341/src/bin/b.rs
+++ abc341/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        mut a: [usize; n],
+        st: [(usize, usize); n - 1],
     }
 
+    for i in 0..n-1 {
+        a[i + 1] += st[i].1 * (a[i] / st[i].0)
+    }
+
+    println!("{}", a[n - 1]);
 }
