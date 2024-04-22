9a76239ed8fa3688bb91fd6b8854b780dbc25630
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-22 20:16:32 +0900

    a

--- abc341/src/bin/a.rs
+++ abc341/src/bin/a.rs
@@ -1,8 +1,12 @@
 use proconio::input;
+use std::iter::repeat;
 
 fn main() {
     input! {
-
+        n: usize,
     }
 
+    let res = repeat("10").take(n).collect::<String>() + "1";
+    // let res = (0..n).map(|_| "10").collect::<String>() + "1";
+    println!("{}", res);
 }
