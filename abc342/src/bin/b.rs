18452b7dee1a957e515727f0c75082dee5cb786e
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-22 20:03:38 +0900

    b

--- abc342/src/bin/b.rs
+++ abc342/src/bin/b.rs
@@ -1,8 +1,21 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-
+        n: usize,
+        p: [usize; n],
+        q: usize,
+        a: [(usize, usize); q],
     }
 
+    let mut hashmap = HashMap::new();
+    for (i, person) in p.iter().enumerate() {
+        hashmap.insert(person, i);
+    }
+
+    for (first, second) in a {
+        let res = hashmap.get(&first).min(hashmap.get(&second));
+        println!("{:?}", p[*res.unwrap()]);
+    }
 }
