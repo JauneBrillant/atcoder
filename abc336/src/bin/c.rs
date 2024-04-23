818a6b1d881f4e42e4dadcf0b1084bb2b91af7bb
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-24 02:19:35 +0900

    c

--- abc336/src/bin/c.rs
+++ abc336/src/bin/c.rs
@@ -2,7 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: i64,
     }
 
+    let quinary = to_quinary(n - 1);
+    println!("{}", quinary * 2);
+}
+
+fn to_quinary(mut n: i64) -> i64 {
+    let mut res = 0;
+    let mut base = 1;
+
+    while n != 0 {
+        let digit = n % 5;
+        res += digit * base;
+        n /= 5;
+        base *= 10;
+    }
+
+    res
 }
