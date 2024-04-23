28572b200df606c7bb3296aad5552bfe1f6387a9
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-23 20:45:47 +0900

    b

--- abc337/src/bin/b.rs
+++ abc337/src/bin/b.rs
@@ -1,8 +1,11 @@
 use proconio::input;
+use regex::Regex;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let re = Regex::new(r"^A*B*C*$").unwrap();
+    println!("{}", if re.is_match(&s) { "Yes" } else { "No" });
 }
