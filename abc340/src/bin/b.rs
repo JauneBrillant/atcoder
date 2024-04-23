982bdc2f24b85ae9fafe5ef691f8f5275239a80c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-23 17:32:53 +0900

    340: b

--- abc340/src/bin/b.rs
+++ abc340/src/bin/b.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        q: usize,
+        query: [(i32, i32); q],
     }
 
+    let mut res: Vec<i32> = Vec::new();
+    for (first, second) in query {
+        if first == 1 {
+            res.push(second);
+        } else {
+            println!("{}", res[res.len() - second as usize])
+        }
+    }
 }
