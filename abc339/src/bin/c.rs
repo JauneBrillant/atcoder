d5f2cbd979d32ba9da82e90caf7491539bb3c3e7
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-23 20:02:15 +0900

    c

--- abc339/src/bin/c.rs
+++ abc339/src/bin/c.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [i64; n],
     }
 
+    let mut total = 0;
+    for num in a {
+        total += num;
+        if total < 0 {
+            total = 0;
+        }
+    }
+    println!("{}", total);
 }
