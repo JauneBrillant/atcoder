a04636e7cd941208cf76fd6fed1084cef8ea406b
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-23 20:41:26 +0900

    a

--- abc337/src/bin/a.rs
+++ abc337/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        xy: [(usize, usize); n],
     }
 
+    let mut total_x = 0;
+    let mut total_y = 0;
+    for (x, y) in xy {
+        total_x += x;
+        total_y += y;
+    }
+
+    println!("{}", if total_x == total_y { "Draw" } else if total_x > total_y { "Takahashi" } else { "Aoki" });
 }
