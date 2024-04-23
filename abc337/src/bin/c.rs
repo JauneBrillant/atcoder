72b05118c8b07c65f81bb890f1c8378f08ab98a0
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-24 00:56:23 +0900

    c

--- abc337/src/bin/c.rs
+++ abc337/src/bin/c.rs
@@ -1,8 +1,26 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [i32; n],
     }
 
+    let mut res: Vec<i32> = vec![0; n];
+    let mut hashmap = HashMap::new();
+    for (i, &v) in a.iter().enumerate() {
+        hashmap.insert(v, (i + 1) as i32);
+        if v == -1 {
+            res[0] = (i + 1) as i32;
+        }
+    }
+
+    for i in 1..n {
+        res[i] = *hashmap.get(&res[i - 1]).unwrap();
+    }
+
+    for num in res {
+        print!("{} ", num);
+    }
 }
