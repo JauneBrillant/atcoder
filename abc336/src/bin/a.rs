7f997a04b8cec308a52eb7eec9b7cbac3b93303c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-24 01:52:40 +0900

    2a

--- abc336/src/bin/a.rs
+++ abc336/src/bin/a.rs
@@ -1,16 +1,17 @@
 use proconio::input;
-use std::iter::*;
+// use std::iter::*;
 
 fn main() {
     input! {
         n: usize,
     }
 
-    let res: String = once('L')
-        .chain(repeat('o').take(n))
-        .chain(once('n'))
-        .chain(once('g'))
-        .collect();
+    // let res: String = once('L')
+    //     .chain(repeat('o').take(n))
+    //     .chain(once('n'))
+    //     .chain(once('g'))
+    //     .collect();
 
+    let res = format!("L{}ng", "o".repeat(n));
     println!("{}", res);
 }
