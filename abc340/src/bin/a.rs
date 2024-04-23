e5eeb57bf1389217c9308602fa5d5974dd02920c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-23 17:19:04 +0900

    340: a

--- abc340/src/bin/a.rs
+++ abc340/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        a: usize,
+        b: usize,
+        d: usize,
     }
 
+    let mut num = a;
+    while num <= b {
+        print!("{}", num);
+        if num == b { println!("\n") } else { print!(" ") };
+        num += d;
+    }
 }
