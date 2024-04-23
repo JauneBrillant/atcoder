ee0f014da4fc5f663e9312d0beda182b27867e71
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-24 01:20:14 +0900

    b

--- abc336/src/bin/b.rs
+++ abc336/src/bin/b.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: i32,
     }
 
+    let binary_string = format!("{:b}", n);
+    let mut res = 0;
+    for c in binary_string.chars().rev() {
+        if c != '0' {
+            break;
+        }
+        res += 1;
+    }
+
+    println!("{}", res);
 }
