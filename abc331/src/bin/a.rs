6635e551444f48998a65e7d2f043fd4df23d54dc
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 21:11:13 +0900

    a

--- abc331/src/bin/a.rs
+++ abc331/src/bin/a.rs
@@ -2,7 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        m: usize,
+        d: usize,
+        mut year: usize,
+        mut month: usize,
+        mut day: usize,
     }
 
+    // 年末
+    if month == m && day == d {
+        println!("{} {} {}", year + 1, 1, 1);
+    }
+    // 月末
+    else if day == d {
+        println!("{} {} {}", year, month + 1, 1);
+    }
+    // その他
+    else {
+        println!("{} {} {}", year, month, day + 1);
+    }
 }
