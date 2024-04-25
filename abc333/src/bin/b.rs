6e18c260209e4370e31aab2860aa25802255031a
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 17:20:24 +0900

    b

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
+        t: String,
     }
 
+    let a = is_long(&s);
+    let b = is_long(&t);
+
+    println!("{}", if a == b { "Yes" } else { "No" });
+}
+
+fn is_long(s: &str) -> bool {
+    let diff = (s.chars().nth(0).unwrap() as i32 - s.chars().nth(1).unwrap() as i32).abs();
+    diff == 2 || diff == 3
 }
