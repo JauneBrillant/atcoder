21757220746b1bac0eb055693d0038fef8ae0f35
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 15:27:15 +0900

    c

--- abc346/src/bin/c.rs
+++ abc346/src/bin/c.rs
@@ -1,8 +1,21 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-
+        n: usize,
+        k: i64,
+        arr: [i64; n],
     }
 
+    let mut st = HashSet::new();
+    for &num in arr.iter() {
+        if num <= k {
+            st.insert(num);
+        }
+    }
+
+    let total: i64 = k * (k + 1) / 2;
+    let res = total - st.iter().sum::<i64>();
+    println!("{}", res);
 }
