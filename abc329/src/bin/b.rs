783acecbf4798883431359eaa330c9ede13723f2
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-26 08:32:30 +0900

    b

--- abc329/src/bin/b.rs
+++ abc329/src/bin/b.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        mut a: [usize; n],
     }
 
+    a.sort();
+    let max = a[a.len() - 1];
+    let mut second_max = 0;
+    for i in (0..a.len()).rev() {
+        if max != a[i] {
+            second_max = a[i];
+            break;
+        }
+    }
+
+    println!("{}", second_max);
 }
