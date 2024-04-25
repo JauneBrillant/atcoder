ad8b41862de2507a05bc4bd4c9da30d827347c2d
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-26 06:14:00 +0900

    b

--- abc331/src/bin/b.rs
+++ abc331/src/bin/b.rs
@@ -2,7 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        s: usize,
+        m: usize,
+        l: usize,
     }
 
+    let mut res = usize::MAX;
+    for i in 0..18 {
+        for j in 0..18 {
+            for k in 0..18 {
+                if 6 * i + 8 * j + 12 * k  >= n {
+                    res = res.min(s * i + m * j + l * k);
+                }
+            }
+        }
+    }
+    println!("{}", res);
 }
