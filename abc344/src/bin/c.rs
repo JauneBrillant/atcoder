78f6990a2b244d934666107cb279269713915de6
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 15:57:12 +0900

    c

--- abc344/src/bin/c.rs
+++ abc344/src/bin/c.rs
@@ -1,8 +1,28 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [i64; n],
+        m: usize,
+        b: [i64; m],
+        l: usize,
+        c: [i64; l],
+        q: usize,
+        x: [i64; q],
     }
 
+    let mut set = HashSet::new();
+    for ai in a.iter() {
+        for bj in b.iter() {
+            for ck in c.iter() {
+                set.insert(ai + bj + ck);
+            }
+        }
+    }
+
+    for num in x {
+        println!("{}", if set.contains(&num) { "Yes" } else { "No" });
+    }
 }
