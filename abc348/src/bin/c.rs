07c5fa4e49707533d8273898be549030d38dacbc
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 14:59:38 +0900

    c

--- abc348/src/bin/c.rs
+++ abc348/src/bin/c.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        beans: [(usize, usize); n],
+    }
+
+    let mut hashmap = HashMap::new();
+    for (a, c) in beans {
+        if let Some(&prev_a) = hashmap.get(&c) {
+            if prev_a > a {
+                hashmap.insert(c, a);
+            }
+        } else {
+            hashmap.insert(c, a);
+        }
+    }
+
+    let mut res = 0;
+    for &v in hashmap.values() {
+        if res < v {
+            res = v;
+        }
+    }
+    println!("{}", res);
 }
