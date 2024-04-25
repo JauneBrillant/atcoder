3ef24c70bc7207e5a6dffb6deb37c8a95e95b058
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 17:24:08 +0900

    refactor: b

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -6,13 +6,13 @@ fn main() {
         t: String,
     }
 
+    let is_long = |s: &str| -> bool {
+        let diff = (s.chars().nth(0).unwrap() as i32 - s.chars().nth(1).unwrap() as i32).abs();
+        diff == 1 || diff == 4
+    };
+
     let a = is_long(&s);
     let b = is_long(&t);
 
     println!("{}", if a == b { "Yes" } else { "No" });
-}
-
-fn is_long(s: &str) -> bool {
-    let diff = (s.chars().nth(0).unwrap() as i32 - s.chars().nth(1).unwrap() as i32).abs();
-    diff == 2 || diff == 3
 }
