1a965ab890ca9eefe03df9ad2fdd79c048134622
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 14:34:33 +0900

    c

--- abc349/src/bin/c.rs
+++ abc349/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+        t: String,
+    }
+
+    let res = is_subarray(&(s.to_ascii_uppercase() + "X"), &t);
+    println!("{}", if res { "Yes" } else { "No" });
+}
+
+fn is_subarray(s: &str, t: &str) -> bool {
+    let mut t_index = 0;
+    for c in s.chars() {
+        if c == t.chars().nth(t_index).unwrap() {
+            t_index += 1;
+        }
+
+        if t_index == t.len() {
+            return true;
+        }
+    }
+    false
 }
