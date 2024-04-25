ad6eb1061c11ed09258f42c121c5143fd7382961
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 16:12:06 +0900

    a

--- abc335/src/bin/a.rs
+++ abc335/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        mut s: String,
     }
 
+    if let Some(last_char) = s.pop() {
+        if last_char != '4' {
+            s.push('4');
+        }
+    }
+    println!("{}", s);
 }
