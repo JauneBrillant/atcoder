9e645546dffdade16c453aaa3b0f17ba65bcdbe7
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-26 08:28:27 +0900

    a

--- abc329/src/bin/a.rs
+++ abc329/src/bin/a.rs
@@ -2,7 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let res = s.chars().map(|c| c.to_string()).collect::<Vec<String>>().join(" ");
+    println!("{}", res);
 }
