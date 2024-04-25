121cf559161750afe6348edc9a595a95cce82a6e
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-25 18:41:48 +0900

    a

--- abc332/src/bin/a.rs
+++ abc332/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        s: i64,
+        k: i64,
+        items: [(i64, i64); n],
     }
 
+    let mut total = items.iter().map(|(price, amount)| price * amount).sum::<i64>();
+    if total < s { total += k }
+    println!("{}", total);
 }
