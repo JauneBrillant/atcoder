36c9a1f77b94ae9670a6c3458279d4e38eff9c5c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-26 19:51:58 +0900

    a

--- abc328/src/bin/a.rs
+++ abc328/src/bin/a.rs
@@ -2,7 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        x: usize,
+        s: [usize; n],
     }
 
+    let res: usize = s.iter().filter(|&v| *v <= x).sum();
+    println!("{}", res);
 }
