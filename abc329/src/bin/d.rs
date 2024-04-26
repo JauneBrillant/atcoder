39ae0a9bd3379e54d6eab25a1985e074d3615cd0
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-26 19:45:05 +0900

    d

--- abc329/src/bin/d.rs
+++ abc329/src/bin/d.rs
@@ -1,8 +1,26 @@
 use proconio::input;
+use std::collections::BTreeMap;
 
 fn main() {
     input! {
-
+        _n: usize,
+        m: usize,
+        votes: [usize; m],
     }
 
+    let mut tree_map = BTreeMap::new();
+    let mut winner = (0, 0);
+    for vote in votes.iter() {
+        *tree_map.entry(vote).or_insert(0) += 1;
+        if let Some(count) = tree_map.get(&vote) {
+            if *count == winner.1 && *vote < winner.0 {
+                winner = (*vote, *count);
+            }
+
+            if *count > winner.1 {
+                winner = (*vote, *count);
+            }
+        }
+        println!("{}", winner.0);
+    }
 }
