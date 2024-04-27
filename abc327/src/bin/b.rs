accc3f2d824e811e44f94040db86d22b61b91995
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 17:31:57 +0900

    b

--- abc327/src/bin/b.rs
+++ abc327/src/bin/b.rs
@@ -2,7 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        b: i64,
     }
 
+    if let Some(i) = (1..=15 as i64).find(|&v| v.pow(v as u32) == b) {
+        println!("{}", i);
+    } else {
+        println!("{}", -1);
+    }
 }
