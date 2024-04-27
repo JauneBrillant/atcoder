05479c4f34b6728f7d771bf8e4cbfaf4e1d9415b
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 18:58:11 +0900

    b

--- abc326/src/bin/b.rs
+++ abc326/src/bin/b.rs
@@ -2,7 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: i32,
     }
 
+    for i in n..i32::MAX {
+        if is_326_like_number(i) {
+            println!("{}", i);
+            return;
+        }
+    }
+}
+
+fn is_326_like_number(mut num: i32) -> bool {
+    let one = num % 10;
+    num /= 10;
+    let ten = num % 10;
+    num /= 10;
+    let handred = num;
+
+    handred * ten == one
 }
