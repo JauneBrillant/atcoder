b00a6f0a5e60e12f7c0c6d6eff0932d0cc22da4d
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 16:54:25 +0900

    c

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -1,8 +1,27 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        n: usize,
+        q: usize,
+        s: Chars,
+        lr: [(usize, usize); q],
     }
 
+    let mut a = vec![0; n - 1];
+    for i in 0..n - 1 {
+        a[i] = (s[i] == s[i + 1]) as usize;
+    }
+
+    let mut sum: Vec<usize> = vec![0; n];
+    for i in 0..n - 1 {
+        sum[i + 1] = sum[i] + a[i];
+    }
+
+    for i in 0..q {
+        let (left, right) = lr[i];
+        let res = sum[right - 1] - sum[left - 1];
+        println!("{}", res);
+    }
 }
