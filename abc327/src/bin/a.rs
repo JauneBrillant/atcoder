181eb9b269db21ce181d4572580eff2a772004c2
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 17:02:25 +0900

    a

--- abc327/src/bin/a.rs
+++ abc327/src/bin/a.rs
@@ -1,8 +1,17 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: Chars,
     }
 
+    for i in 0..n - 1 {
+        if a[i] == 'a' && a[i + 1] == 'b' || a[i] == 'b' && a[i + 1] == 'a' {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No")
 }
