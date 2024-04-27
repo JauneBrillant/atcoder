d584e1c3d28084c7d2977a60b555d1fd1c50ed14
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 15:43:47 +0900

    b

--- abc328/src/bin/b.rs
+++ abc328/src/bin/b.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
     }
 
+    let mut res = 0;
+    for i in 1..=n {
+        for j in 1..=a[i - 1] {
+            let day = format!("{}{}", i, j);
+            let is_all_same = day.chars().all(|c| c == day.chars().next().unwrap());
+            res += is_all_same as usize;
+        }
+    }
+
+    println!("{}", res);
 }
