a4289cfa679715775197f6dc5245df60a0550345
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 23:21:24 +0900

    ref: b

--- abc351/src/bin/b.rs
+++ abc351/src/bin/b.rs
@@ -1,17 +1,20 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
         n: usize,
-        grid_a: [String; n],
-        grid_b: [String; n],
+        grid_a: [Chars; n],
+        grid_b: [Chars; n],
     }
 
     for i in 0..n {
         for j in 0..n {
-            if grid_a[i].chars().nth(j) != grid_b[i].chars().nth(j) {
+            if grid_a[i][j] != grid_b[i][j] {
                 println!("{} {}", i + 1, j + 1);
+                return;
             }
         }
     }
+    unreachable!();
 }
