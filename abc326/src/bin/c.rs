1478a05df56c2185b961aac57a6fa23f5845c85b
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-27 19:42:19 +0900

    c

--- abc326/src/bin/c.rs
+++ abc326/src/bin/c.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+    }
+    a.sort();
 
+    let mut res = 0;
+    let mut r = 0;
+    for l in 0..n {
+        while r < n && a[r as usize] < a[l] + m {
+            r += 1;
+        }
+        res = res.max(r - l);
     }
 
+    println!("{}", res);
 }
