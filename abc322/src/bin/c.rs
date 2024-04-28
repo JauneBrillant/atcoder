42887f7bab3b60f58063be9d2b534378c26775b5
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 17:00:36 +0900

    c

--- abc322/src/bin/c.rs
+++ abc322/src/bin/c.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        m: usize,
+        a: [usize; m],
     }
 
+    let mut days = vec![false; n];
+    for day in a {
+        days[day - 1] = true;
+    }
+
+    let mut ans = vec![0; n];
+    let mut cnt = 0;
+    for i in (0..n).rev() {
+        cnt += 1;
+        if days[i] {
+            cnt = 0;
+        }
+        ans[i] = cnt;
+    }
+
+    for v in ans {
+        println!("{}", v);
+    }
 }
