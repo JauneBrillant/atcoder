3e4337b4c33858fed3900b1b15f676dce34b6514
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 11:27:51 +0900

    b

--- abc325/src/bin/b.rs
+++ abc325/src/bin/b.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        wx: [(usize, usize); n],
     }
 
+    let mut max_people = 0;
+    for h in 0..24 {
+        let mut total = 0;
+        for (people, time) in wx.iter() {
+            let utc_time = (h + time) % 24;
+            if 9 <= utc_time && utc_time < 18 { total += people; }
+        }
+        max_people = max_people.max(total);
+    }
+
+    println!{"{}", max_people};
 }
