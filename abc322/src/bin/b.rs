c92e05b0fa6588a4f80423d01f6de17ddb64d462
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 16:29:11 +0900

    b

--- abc322/src/bin/b.rs
+++ abc322/src/bin/b.rs
@@ -2,7 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        _n: usize,
+        _m: usize,
+        s: String,
+        t: String,
     }
 
+    if is_head(&s, &t) && is_tail(&s, &t) {
+        println!("0");
+    } else if is_head(&s, &t) && !is_tail(&s, &t) {
+        println!("1");
+    } else if !is_head(&s, &t) && is_tail(&s, &t) {
+        println!("2");
+    } else {
+        println!("3");
+    }
+}
+
+fn is_head(s: &str, t: &str) -> bool {
+    *s == t[0..s.len()]
+}
+
+fn is_tail(s: &str, t: &str) -> bool {
+    let l = t.len() - s.len();
+    t[l..] == *s
 }
