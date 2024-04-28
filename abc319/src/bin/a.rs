825255eaa66438cd44a3dca2e2b01a10001b9897
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 19:22:12 +0900

    ref: a

--- abc319/src/bin/a.rs
+++ abc319/src/bin/a.rs
@@ -1,26 +1,29 @@
-use proconio::input;
 use std::collections::HashMap;
+
+use proconio::input;
 
 fn main() {
     input! {
         s: String,
     }
 
-    let mut hashmap: HashMap<String, usize> = HashMap::new();
-    hashmap.insert(String::from("tourist"), 3858);
-    hashmap.insert(String::from("ksun48"), 3679);
-    hashmap.insert(String::from("Benq"), 3658);
-    hashmap.insert(String::from("Um_nik"), 3648);
-    hashmap.insert(String::from("apiad"), 3638);
-    hashmap.insert(String::from("Stonefeang"), 3630);
-    hashmap.insert(String::from("ecnerwala"), 3613);
-    hashmap.insert(String::from("mnbvmar"), 3555);
-    hashmap.insert(String::from("newbiedmy"), 3516);
-    hashmap.insert(String::from("semiexp"), 3481);
+    const DATA: [(&str, usize); 10] = [
+        ("tourist", 3858),
+        ("ksun48", 3679),
+        ("Benq", 3658),
+        ("Um_nik", 3648),
+        ("apiad", 3638),
+        ("Stonefeang", 3630),
+        ("ecnerwala", 3613),
+        ("mnbvmar", 3555),
+        ("newbiedmy", 3516),
+        ("semiexp", 3481),
+    ];
 
-    if let Some(rate) = hashmap.get(&s) {
+    let hashmap: HashMap<&str, usize> = DATA.iter().cloned().collect();
+    if let Some(rate) = hashmap.get(&*s) {
         println!("{}", rate);
     } else {
-        // pass
+
     }
 }
