76c16cef7172d939697e5d7abe00663d1581c581
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 17:26:21 +0900

    a

--- abc321/src/bin/a.rs
+++ abc321/src/bin/a.rs
@@ -1,8 +1,17 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        n: Chars,
     }
 
+    for i in 0..n.len() - 1 {
+        if n[i] <= n[i + 1] {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
