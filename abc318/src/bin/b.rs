e1536adee59ca8ca4d05b96bf23a1732b596d592
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 21:35:57 +0900

    b

--- abc318/src/bin/b.rs
+++ abc318/src/bin/b.rs
@@ -2,7 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        m: [[usize; 4]; n],
     }
 
+    let mut grid = vec![vec![false; 100]; 100];
+    for points in m {
+        let x1 = points[0];
+        let x2 = points[1];
+        let y1 = points[2];
+        let y2 = points[3];
+
+        for i in x1..x2 {
+            for j in y1..y2 {
+                grid[i][j] = true;
+            }
+        }
+    }
+
+    let res = grid.iter().flatten().filter(|&&e| e == true).count();
+    println!("{}", res);
 }
