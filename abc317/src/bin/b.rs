991243972f34e91e1c23b3e4ef797713a94b442a
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 21:56:54 +0900

    ref: b

--- abc317/src/bin/b.rs
+++ abc317/src/bin/b.rs
@@ -7,10 +7,16 @@ fn main() {
     }
 
     a.sort();
-    for window in a.windows(2) {
-        if window[1] - window[0] != 1 {
-            println!("{}", (window[0] + 1));
-            return;
-        }
+    let missing_number = a.windows(2)
+        .find_map(|window| {
+            if window[1] - window[0] != 1 {
+                Some(window[0] + 1)
+            } else {
+                None
+            }
+        });
+
+    if let Some(res) = missing_number {
+        println!("{}", res);
     }
 }
