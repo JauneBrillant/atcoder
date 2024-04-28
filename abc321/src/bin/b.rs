be6ebb3a504f40d6caaaee87ccb04e43e34b31c7
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 18:09:17 +0900

    b

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -2,7 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        x: i32,
+        a: [i32; n - 1],
     }
 
+    let mut res = i32::MAX;
+    for i in 0..=100 {
+        let mut arr = a.clone();
+        arr.push(i);
+
+        let total = arr.iter().sum::<i32>();
+        let min = *arr.iter().min().unwrap();
+        let max = *arr.iter().max().unwrap();
+
+        if total - (max + min) >= x {
+            res = res.min(i);
+        }
+    }
+
+    if res == i32::MAX {
+        println!("{}", -1)
+    } else {
+        println!("{}", res);
+    }
 }
