7b532260884dfb21e51e58d80620c913039b3e5c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 17:43:38 +0900

    add another solution: a

--- abc321/src/bin/a.rs
+++ abc321/src/bin/a.rs
@@ -1,14 +1,36 @@
 use proconio::input;
-use proconio::marker::Chars;
+// use proconio::marker::Chars;
+
+// fn main() {
+//     input! {
+//         n: Chars,
+//     }
+
+//     if n.windows(2).any(|w| w[0] <= w[1]) {
+//         println!("No");
+//     } else {
+//         println!("Yes");
+//     }
+// }
 
 fn main() {
     input! {
-        n: Chars,
+        mut n: i32,
     }
 
-    if n.windows(2).any(|w| w[0] <= w[1]) {
-        println!("No");
-    } else {
-        println!("Yes");
+    let mut prev_digit = n % 10;
+    n /= 10;
+    while n > 0 {
+        let curr_digit = n % 10;
+
+        // println!("curr_digit = {} : prev_digit = {}", curr_digit, prev_digit);
+
+        if curr_digit <= prev_digit {
+            println!("No");
+            return;
+        }
+        n /= 10;
+        prev_digit = curr_digit;
     }
+    println!("Yes");
 }
