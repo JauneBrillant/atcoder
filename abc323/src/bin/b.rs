6825a1c81755f9d467ad7ae6c69765e97cbc9c9c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 13:01:59 +0900

    b

--- abc323/src/bin/b.rs
+++ abc323/src/bin/b.rs
@@ -1,8 +1,21 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        n: usize,
+        x: [Chars; n],
     }
 
+    let mut arr = vec![];
+    for (i, str) in x.iter().enumerate() {
+        let win_cnt = str.iter().filter(|&&c| c == 'o').count();
+        arr.push((i + 1, win_cnt));
+    }
+
+    arr.sort_by_key(|&(_, win_cnt)| std::cmp::Reverse(win_cnt));
+
+    for (people, _) in arr {
+        print!("{} ", people);
+    }
 }
