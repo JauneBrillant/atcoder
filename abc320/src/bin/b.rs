5000102a700f826d8a25a85f4da8134e3ee291e4
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-28 18:48:21 +0900

    b

--- abc320/src/bin/b.rs
+++ abc320/src/bin/b.rs
@@ -1,8 +1,28 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let mut st = HashSet::new();
+    for i in 0..s.len() {
+        for j in i..=s.len() {
+            st.insert(&s[i..j]);
+        }
+    }
+
+    let mut res = usize::MIN;
+    for e in st {
+        if is_palindrome(e) {
+            res = res.max(e.len());
+        }
+    }
+
+    println!("{}", res);
+}
+
+fn is_palindrome(str: &str) -> bool {
+    str.chars().eq(str.chars().rev())
 }
