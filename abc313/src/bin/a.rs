1e0362396a56f236ad650de19acb1e38794b459b
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-29 19:08:34 +0900

    a

--- abc313/src/bin/a.rs
+++ abc313/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        p: [i32; n],
     }
 
+    if n == 1 {
+        println!("0");
+        return;
+    }
+
+    let goal = p.iter().skip(1).max().unwrap() + 1;
+    let res = goal - p[0];
+    println!("{}", if p[0] < goal { res } else { 0 });
 }
