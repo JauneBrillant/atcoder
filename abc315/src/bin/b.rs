3309fc47f3b1b5b716469f9d612ac3382e305515
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-29 15:19:31 +0900

    b

--- abc315/src/bin/b.rs
+++ abc315/src/bin/b.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        m: usize,
+        d: [usize; m],
     }
 
+    let total_days = d.iter().sum::<usize>();
+    let mut day = total_days / 2 + 1;
+
+    let mut i = 0;
+    while day > d[i] {
+        day -= d[i];
+        i += 1;
+    }
+
+    println!("{} {}", i + 1, day);
 }
