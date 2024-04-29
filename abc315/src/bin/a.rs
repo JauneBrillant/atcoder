da49031970a22908ce56a9873f6d6b626c55fe44
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-29 15:09:00 +0900

    a

--- abc315/src/bin/a.rs
+++ abc315/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let res = s.chars().filter(|&c|
+        c != 'a' &&
+        c != 'i' &&
+        c != 'u' &&
+        c != 'e' &&
+        c != 'o'
+    ).collect::<String>();
+
+    println!("{}", res);
 }
