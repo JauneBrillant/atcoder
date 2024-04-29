7cb2468bacfcd25d7d80c164c6470c453d7956e5
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-29 19:46:11 +0900

    b

--- abc313/src/bin/b.rs
+++ abc313/src/bin/b.rs
@@ -1,8 +1,25 @@
+use std::collections::HashSet;
 use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
     }
 
+    let mut set = HashSet::new();
+    for i in 1..=n {
+        set.insert(i);
+    }
+
+    for (_, b) in ab.iter() {
+        set.remove(b);
+    }
+
+    if set.len() >= 2 {
+        println!("-1");
+    } else {
+        println!("{}", set.iter().nth(0).unwrap());
+    }
 }
