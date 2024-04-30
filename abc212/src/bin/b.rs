9a390f8d70fb0ce4c4a5789e55db4dcde16e2666
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-01 01:43:31 +0900

    b

--- abc212/src/bin/b.rs
+++ abc212/src/bin/b.rs
@@ -1,20 +1,15 @@
 use proconio::input;
-use proconio::marker::Chars;
 
 fn main() {
     input! {
-        pass: Chars,
+        pass: String,
     }
 
-    let is_all_same = pass.iter().all(|e| *e == pass[0]);
-    let is_weak = pass.windows(2).all(|e| {
-        e[0] == '9' && e[1] == '0' ||
-        (e[0].to_digit(10).unwrap() + 1) == e[1].to_digit(10).unwrap()
-    });
+    let res = is_weak(&pass);
+    println!("{}", if res { "Weak" } else { "Strong" });
+}
 
-    if is_all_same || is_weak {
-        println!("Weak");
-    } else {
-        println!("Strong");
-    }
+fn is_weak(pass: &str) -> bool {
+    let digit: Vec<_> = pass.chars().map(|e| e.to_digit(10).unwrap()).collect();
+    digit.iter().all(|&e| e == digit[0]) || digit.windows(2).all(|e| (e[0] + 1) % 10 == e[1])
 }
