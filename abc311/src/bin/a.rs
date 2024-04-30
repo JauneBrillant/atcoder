7667ea7279d7fe20b5cdc84d170a6ea8f432b615
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-30 18:49:36 +0900

    a

--- abc311/src/bin/a.rs
+++ abc311/src/bin/a.rs
@@ -1,8 +1,25 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        _n: usize,
+        s: Chars,
     }
 
+    let mut cnt_a = false;
+    let mut cnt_b = false;
+    let mut cnt_c = false;
+    for (i, c) in s.iter().enumerate() {
+        match c {
+            'A' => cnt_a = true,
+            'B' => cnt_b = true,
+            'C' => cnt_c = true,
+            _ => (),
+        }
+        if cnt_a && cnt_b && cnt_c {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }
