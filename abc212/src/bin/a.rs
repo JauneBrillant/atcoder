c3af7bd18b9007a4469628100f2e1a469f15eea7
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-01 01:10:39 +0900

    a

--- abc212/src/bin/a.rs
+++ abc212/src/bin/a.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        a: i32,
+        b: i32,
     }
 
+    match (a, 0 < a, b, 0 < b) {
+        (_, true, 0, _) => println!("Gold"),
+        (0, _, _, true) => println!("Silver"),
+        (_, true, _, true) => println!("Alloy"),
+        _ => unreachable!(),
+    }
 }
