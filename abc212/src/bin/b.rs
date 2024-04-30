9a8300f5c1bcf9d2ec54b9abece84819e1e3aefa
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-01 01:26:37 +0900

    b

--- abc212/src/bin/b.rs
+++ abc212/src/bin/b.rs
@@ -1,8 +1,20 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        pass: Chars,
     }
 
+    let is_all_same = pass.iter().all(|e| *e == pass[0]);
+    let is_weak = pass.windows(2).all(|e| {
+        e[0] == '9' && e[1] == '0' ||
+        (e[0].to_digit(10).unwrap() + 1) == e[1].to_digit(10).unwrap()
+    });
+
+    if is_all_same || is_weak {
+        println!("Weak");
+    } else {
+        println!("Strong");
+    }
 }
