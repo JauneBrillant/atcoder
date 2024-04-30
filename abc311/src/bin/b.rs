9bbeb9a6cf3f170a27e30fd56fb491474af15a04
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-30 19:13:09 +0900

    b

--- abc311/src/bin/b.rs
+++ abc311/src/bin/b.rs
@@ -1,8 +1,27 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        n: usize,
+        d: usize,
+        s: [Chars; n],
     }
 
+    let mut res = 0;
+    let mut streak = 0;
+    for i in 0..d {
+        for j in 0..n {
+            if s[j][i] == 'x' {
+                streak = 0;
+                break;
+            }
+            if j == n - 1 {
+                streak += 1;
+            }
+        }
+        res = res.max(streak);
+    }
+
+    println!("{}", res);
 }
