ad80158e9149253f5b657de2a4326c87890b8e1c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-04-30 19:20:55 +0900

    a

--- abc310/src/bin/a.rs
+++ abc310/src/bin/a.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
+    input! {
+        n: usize,
+        p: i32,
+        q: i32,
+        d: [i32; n],
+    }
 
+    let min = d.iter().min().unwrap();
+    let res = if p < q + min { p } else { q + min };
+    println!("{}", res);
 }
