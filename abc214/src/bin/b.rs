6d6bae7fa72d76a8c075cb5cd5f5cd62f1542d89
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 05:57:58 +0900

    b

--- abc214/src/bin/b.rs
+++ abc214/src/bin/b.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: usize,
+        t: usize,
     }
 
+    let mut res = 0;
+    for i in 0..= s {
+        for j in 0..= s - i {
+            for k in 0..= s - i - j {
+                if i * j * k <= t {
+                    res += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", res);
 }
