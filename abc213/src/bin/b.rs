562f5961bba7b82370ef76396d8358fd2f70e138
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 05:38:59 +0900

    b

--- abc213/src/bin/b.rs
+++ abc213/src/bin/b.rs
@@ -1,8 +1,20 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
     }
 
+    let mut map = HashMap::new();
+    for (i, v) in a.iter().enumerate() {
+        map.insert(v, i + 1);
+    }
+
+    let mut sorted_keys = map.keys().collect::<Vec<_>>();
+    sorted_keys.sort_by(|a, b| b.cmp(a));
+
+    let res = map.get(sorted_keys[1]).unwrap();
+    println!("{}", res);
 }
