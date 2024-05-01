48455d7819d16ac3364ab45ef349bb4647fd1543
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 05:46:17 +0900

    ref: a

--- abc214/src/bin/a.rs
+++ abc214/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
     }
 
+    match n {
+        1..=125 => println!("4"),
+        126..=211 => println!("6"),
+        212..=214 => println!("8"),
+        _ => (),
+    }
 }
