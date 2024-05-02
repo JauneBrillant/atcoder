78be4e3f7ee80744b7f5286ca7dc8b87b2081045
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 19:41:01 +0900

    a

--- abc220/src/bin/a.rs
+++ abc220/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        a: usize,
+        b: usize,
+        c: usize,
     }
 
+    let mut res = -1;
+    for i in a..=b {
+        if i % c == 0 {
+            res = i as i32;
+        }
+    }
+    println!("{}", res);
 }
