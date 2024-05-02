c1f63890dd45c882fd46d04442676946db682747
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 14:40:27 +0900

    b

--- abc216/src/bin/b.rs
+++ abc216/src/bin/b.rs
@@ -1,8 +1,19 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-
+        n: usize,
+        st: [(String, String); n],
     }
 
+    let mut set = HashSet::new();
+    for name in st.iter() {
+        if set.contains(name) {
+            println!("Yes");
+            return;
+        }
+        set.insert(name);
+    }
+    println!("No");
 }
