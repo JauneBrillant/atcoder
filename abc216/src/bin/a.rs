290814dd85b9b2524cc74d94d3999a9dbfeedb11
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 14:24:49 +0900

    a

--- abc216/src/bin/a.rs
+++ abc216/src/bin/a.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let parts = s.trim().split('.').collect::<Vec<_>>();
+    let x: usize = parts[0].parse().unwrap();
+    let y: usize = parts[1].parse().unwrap();
+
+    match y {
+        0..=2 => println!("{}-", x),
+        3..=6 => println!("{}", x),
+        7..=9 => println!("{}+", x),
+        _ => (),
+    }
 }
