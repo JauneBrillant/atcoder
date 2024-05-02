21b0e61234bc90efb393a445eb88681c26150d75
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:47:51 +0900

    b

--- abc218/src/bin/b.rs
+++ abc218/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        p: [u8; 26],
     }
 
+    let mut res = String::new();
+    for i in 0..26 {
+        let c = (96 + p[i]) as char;
+        res.push(c);
+    }
+
+    println!("{}", res);
 }
