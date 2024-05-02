0d99f67239cf35358f4b7ef530ebd02e98619668
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:34:16 +0900

    c

--- abc217/src/bin/c.rs
+++ abc217/src/bin/c.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        p: [usize; n],
     }
 
+    let mut q = p.clone();
+    for i in 0..n {
+        q[p[i] - 1] = i + 1;
+    }
+
+    for v in q {
+        print!("{} ", v);
+    }
 }
