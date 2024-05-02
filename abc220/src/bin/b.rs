9d10c2e526199d45e4a1ee1647d840760627f3ee
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 22:26:10 +0900

    b

--- abc220/src/bin/b.rs
+++ abc220/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        k: u32,
+        a: String,
+        b: String,
     }
 
+    let aa = usize::from_str_radix(&a, k).unwrap();
+    let bb = usize::from_str_radix(&b, k).unwrap();
+
+    let res = aa * bb;
+    println!("{}", res);
 }
