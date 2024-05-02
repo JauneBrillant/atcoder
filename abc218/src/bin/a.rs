8a99bc60d2b153c1481b22bc92ed374de70b612e
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:37:43 +0900

    a

--- abc218/src/bin/a.rs
+++ abc218/src/bin/a.rs
@@ -2,7 +2,10 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        s: String,
     }
 
+    let res = s.chars().nth(n - 1).unwrap() == 'o';
+    println!("{}", if res { "Yes" } else { "No" });
 }
