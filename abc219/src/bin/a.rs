3b5c730c9f68bbbabfb0d00fbca5756f115ee71d
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:57:07 +0900

    a

--- abc219/src/bin/a.rs
+++ abc219/src/bin/a.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        x: usize,
     }
 
+    match x {
+        0..=39 => println!("{}", 40 - x),
+        40..=69 => println!("{}", 70 - x),
+        70..=89 => println!("{}", 90 - x),
+        90.. => println!("expert"),
+        _ => (),
+    }
 }
