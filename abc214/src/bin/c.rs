0158d362ff9fc6bbfd9ca667cc41ac285599c648
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 12:08:52 +0900

    c

--- abc214/src/bin/c.rs
+++ abc214/src/bin/c.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        s: [usize; n],
+        t: [usize; n],
     }
 
+    let mut memo = t.clone();
+    for i in 0..(n * 2) {
+        memo[(i + 1) % n] = memo[(i + 1) % n].min(memo[i % n] + s[i % n]);
+    }
+
+    for v in memo {
+        println!("{}", v);
+    }
 }
