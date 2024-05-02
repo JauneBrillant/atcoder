f2ae88586cd65332971b3da16f1dd8812e1a3194
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 13:12:32 +0900

    c

--- abc215/src/bin/c.rs
+++ abc215/src/bin/c.rs
@@ -1,8 +1,16 @@
 use proconio::input;
+use itertools::Itertools;
 
 fn main() {
     input! {
-
+        s: String,
+        k: usize,
     }
 
+    let mut permutations = s.chars().sorted().permutations(s.len()).unique();
+
+    if let Some(kth_permutation) = permutations.nth(k - 1) {
+        let res: String = kth_permutation.into_iter().collect();
+        println!("{}", res);
+    }
 }
