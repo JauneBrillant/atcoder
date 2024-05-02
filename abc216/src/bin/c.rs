b6cd24e04317c2c5ea054f380e75eff7b44c21b3
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:06:24 +0900

    c

--- abc216/src/bin/c.rs
+++ abc216/src/bin/c.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        mut n: usize,
     }
 
+    let mut res = String::from("");
+    while n != 0 {
+        if n % 2 == 0 {
+            n /= 2;
+            res += "B";
+        } else {
+            n -= 1;
+            res += "A";
+        }
+    }
+
+    println!("{}", res.chars().rev().collect::<String>());
 }
