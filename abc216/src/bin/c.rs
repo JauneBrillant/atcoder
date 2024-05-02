16bfdbe4652cc048fadd02fbc9e04887fac42139
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:14:23 +0900

    ref: c

--- abc216/src/bin/c.rs
+++ abc216/src/bin/c.rs
@@ -7,12 +7,13 @@ fn main() {
 
     let mut res = String::from("");
     while n != 0 {
-        if n % 2 == 0 {
-            n /= 2;
+        if n & 1 == 0 {
+            n >>= 1;
             res += "B";
         } else {
             n -= 1;
-            res += "A";
+            n >>= 1;
+            res += "AB";
         }
     }
 
