ebac3fd615cad07efce8ed91ca6a1d7b5767ac88
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 15:25:33 +0900

    b

--- abc217/src/bin/b.rs
+++ abc217/src/bin/b.rs
@@ -1,8 +1,22 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-
+        s1: String,
+        s2: String,
+        s3: String,
     }
 
+    let mut set = HashSet::new();
+    set.insert(format!("{}", "ABC"));
+    set.insert(format!("{}", "ARC"));
+    set.insert(format!("{}", "AGC"));
+    set.insert(format!("{}", "AHC"));
+
+    set.remove(&s1);
+    set.remove(&s2);
+    set.remove(&s3);
+
+    println!("{}", set.into_iter().next().unwrap());
 }
