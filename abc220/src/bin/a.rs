d88aa4c83a7ab6bb7b37b6bab81209ee4ba48e83
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 22:01:29 +0900

    ref: a

--- abc220/src/bin/a.rs
+++ abc220/src/bin/a.rs
@@ -2,11 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: f32,
-        b: f32,
-        c: f32,
+        a: i32,
+        b: i32,
+        c: i32,
     }
 
-    let res = ((a / c).ceil()) as i32 * c as i32;
-    println!("{}", if res <= b as i32 { res } else { -1 });
+    let res = (a + c - 1) / c * c;
+    // let res = ((a / c).ceil()) as i32 * c as i32;
+    println!("{}", if res <= b { res } else { -1 });
 }
