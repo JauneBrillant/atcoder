1a0186f8f2f12d54e60f55b418855601a4b27e12
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-02 12:22:27 +0900

    b

--- abc215/src/bin/b.rs
+++ abc215/src/bin/b.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
     }
 
+    let mut res = 0;
+    for i in 0..60 {
+        if 2_usize.pow(i) <= n {
+            res = i;
+        }
+    }
+
+    println!("{}", res);
 }
