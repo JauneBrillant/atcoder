05956730b90bf9c173632f000cabcd509c695ce4
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-03 20:10:22 +0900

    b

--- abc222/src/bin/b.rs
+++ abc222/src/bin/b.rs
@@ -2,7 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        p: usize,
+        a: [usize; n],
     }
 
+    let res = a.iter().filter(|&&v| v < p).count();
+    println!("{}", res);
 }
