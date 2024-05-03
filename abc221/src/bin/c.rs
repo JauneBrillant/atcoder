cb00914dc6360d8d08d8eeb36e390b6749fe4553
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-03 19:07:56 +0900

    c

--- abc221/src/bin/c.rs
+++ abc221/src/bin/c.rs
@@ -1,8 +1,23 @@
 use proconio::input;
+use itertools::Itertools;
 
 fn main() {
     input! {
-
+        n: String,
     }
 
+    let permutations = n.chars().permutations(n.len());
+
+    let mut ans = 0;
+    for perm in permutations {
+        let str = perm.iter().collect::<String>();
+        for i in 1..n.len() {
+            let (l, r) = str.split_at(i);
+            let l: usize = l.parse().unwrap();
+            let r: usize = r.parse().unwrap();
+            ans = ans.max(l * r);
+        }
+    }
+
+    println!("{}", ans);
 }
