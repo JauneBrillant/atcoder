633dcb53e29c9fceb9e59b9cb4e31c3c4a53da5c
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-03 20:05:19 +0900

    a

--- abc222/src/bin/a.rs
+++ abc222/src/bin/a.rs
@@ -3,7 +3,12 @@ use proconio::marker::Chars;
 
 fn main() {
     input! {
-        n: Chars,
+        mut n: Chars,
     }
 
+    for _ in 0..4 - n.len() {
+        n.insert(0, '0');
+    }
+
+    println!("{}", n.iter().collect::<String>());
 }
