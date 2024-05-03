f1cca44d71ce7f2b423dc696b10e75fc0b1ca94f
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-03 17:58:46 +0900

    b

--- abc221/src/bin/b.rs
+++ abc221/src/bin/b.rs
@@ -1,8 +1,22 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
+        s: Chars,
+        t: Chars,
+    }
+    if s == t { println!("Yes"); return}
 
+    for i in 0..s.len() - 1 {
+        let mut s_copy = s.clone();
+        s_copy.swap(i, i + 1);
+
+        if s_copy == t {
+            println!("Yes");
+            return;
+        }
     }
 
+    println!("No");
 }
