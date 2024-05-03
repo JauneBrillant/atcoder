2820adf3ff2f72beef881d4a943fc0576e1a93d0
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-03 17:27:21 +0900

    c

--- abc220/src/bin/c.rs
+++ abc220/src/bin/c.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
+        x: usize,
     }
 
+    let total_a = a.iter().sum::<usize>();
+    let mut res = (x / total_a) * n;
+    let mut total = total_a * (x / total_a);
+    let mut i = 0;
+    while total <= x {
+        total += a[i];
+        res += 1;
+        i += 1;
+    }
+
+    println!("{}", res);
 }
