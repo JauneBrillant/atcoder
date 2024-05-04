a0d50b3011e8b23e5c7b00c5952688f3af4b04ee
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-04 16:28:52 +0900

    a

--- abc223/src/bin/a.rs
+++ abc223/src/bin/a.rs
@@ -4,4 +4,11 @@ fn main() {
     input! {
         x: usize
     }
+    if x == 0 {
+        println!("No");
+        return;
+    }
+
+    let flag = x % 100 == 0;
+    println!("{}", if flag { "Yes" } else { "No" });
 }
