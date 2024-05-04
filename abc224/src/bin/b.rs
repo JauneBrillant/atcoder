f20891825a1c6169231615c30bf1f658fe4b4f9a
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-04 18:10:21 +0900

    b

--- abc224/src/bin/b.rs
+++ abc224/src/bin/b.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
     }
 
+    for i in 0..h - 1 {
+        for j in 0..w - 1 {
+            if a[i][j] + a[i + 1][j + 1] > a[i + 1][j] + a[i][j + 1] {
+                println!("No");
+                return;
+            }
+        }
+    }
+
+    println!("Yes");
 }
