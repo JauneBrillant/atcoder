86a9a6106aa6aac2f0cca4971886f2f3ee0d40f3
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-04 23:15:01 +0900

    c

--- abc352/src/bin/c.rs
+++ abc352/src/bin/c.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        ab: [(usize, usize); n],
     }
 
+    let mut res = 0;
+    let total = ab.iter().map(|(a, _)| a).sum::<usize>();
+    for (a, b) in ab.iter() {
+        res = res.max(total - a + b);
+    }
+
+    println!("{}", res);
 }
