de861941cf23008153a4f4cab7dba7a8a303c739
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-04 23:14:07 +0900

    a

--- abc352/src/bin/a.rs
+++ abc352/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        _n: usize,
+        x: usize,
+        y: usize,
+        z: usize,
     }
 
+    let min = x.min(y);
+    let max = y.max(x);
+    println!("{}", if min <= z && z <= max { "Yes" } else { "No" });
 }
