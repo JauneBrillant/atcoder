c6391c4458b6f7fddcf0faeab445f421b7c44ef1
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-04 23:14:41 +0900

    b

--- abc352/src/bin/b.rs
+++ abc352/src/bin/b.rs
@@ -1,8 +1,26 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        s: Chars,
+        t: Chars,
     }
 
+    let mut si = 0;
+    let mut ti = 0;
+    let mut res = vec![];
+    while si < s.len() && ti < t.len() {
+        if s[si] == t[ti] {
+            res.push(ti + 1);
+            si += 1;
+            ti += 1;
+        } else {
+            ti += 1;
+        }
+    }
+
+    for v in res {
+        print!("{} ", v);
+    }
 }
