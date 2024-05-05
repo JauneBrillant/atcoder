ae1f9dd6135949d1609d6bd744e0e5b417555daa
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 02:56:36 +0900

    b

--- abc228/src/bin/b.rs
+++ abc228/src/bin/b.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        x: usize,
+        a: [usize; n],
     }
 
+    let mut memo = vec![false; n];
+    let mut tar = x - 1;
+    while memo[tar] == false {
+        memo[tar] = true;
+        tar = a[tar] - 1;
+    }
+
+    let res = memo.iter().filter(|&&e| e == true).count();
+    println!("{}", res);
 }
