d65d308175aab170de6a4c7860b26c84e9a0fbeb
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 05:08:28 +0900

    ref: b

--- abc312/src/bin/b.rs
+++ abc312/src/bin/b.rs
@@ -8,64 +8,55 @@ fn main() {
         grid: [Chars; n],
     }
 
-    let mut res = vec![];
-    for i in 0..(n - 8) {
-        for j in 0..(m - 8) {
-            let mut square: Vec<Vec<char>> = vec![];
-            for k in 0..9 {
-                square.push(grid[i + k][j..(j + 9)].to_vec());
-            }
+    let mut res = Vec::new();
+    for i in 0..n - 8 {
+        for j in 0..m - 8 {
+            let square = grid[i..i + 9]
+                .iter()
+                .map(|row| row[j..j + 9].iter().cloned().collect())
+                .collect::<Vec<Vec<char>>>();
 
-            if check(&square) {
-                res.push((i + 1, j + 1));
+            if is_tak_code(&square) {
+                res.push((i, j));
             }
         }
     }
 
     for (i, j) in res {
-        println!("{} {}", i, j);
+        println!("{} {}", i + 1, j + 1);
     }
 }
 
-fn check(grid: &[Vec<char>]) -> bool {
+fn is_tak_code(grid: &Vec<Vec<char>>) -> bool {
     for i in 0..3 {
         for j in 0..3 {
-            if grid[i][j] != '#' {
+            if grid[i][j] == '.' {
                 return false;
             }
-        }
-    }
 
-    for i in 0..3 {
-        for j in 0..3 {
-            if grid[8 - i][8 - j] != '#' {
-                return false
-            }
-        }
-    }
-
-    for i in 0..4 {
-        for j in 0..4 {
-            if i < 3 && j < 3 {
-                continue;
-            }
-
-            if grid[i][j] != '.' {
+            if grid[8 - i][8 - j] == '.' {
                 return false;
             }
         }
     }
 
     for i in 0..4 {
-        for j in 0..4 {
-            if i < 3 && j < 3 {
-                continue;
-            }
+        if grid[i][3] == '#' {
+            return false;
+        }
 
-            if grid[8 - i][8 - j] != '.' {
-                return false;
-            }
+        if grid[3][i] == '#' {
+            return false;
+        }
+
+        if grid[8 - i][5] == '#' {
+            return false;
+        }
+
+        if grid[5][8 - i] == '#' {
+            return false;
         }
     }
+
     true
 }
