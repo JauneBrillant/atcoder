fefd53909e23466dfaa9db47cfa457fc82e03576
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 06:09:18 +0900

    ref: a

--- abc229/src/bin/a.rs
+++ abc229/src/bin/a.rs
@@ -1,25 +1,14 @@
 use proconio::input;
-use proconio::marker::Chars;
 
 fn main() {
     input! {
-        s1: Chars,
-        s2: Chars,
+        s1: String,
+        s2: String,
     }
 
-    let mut cnt = 0;
-    for i in 0..2 {
-        if s1[i] == '#' {
-            cnt += 1;
-        }
-        if s2[i] == '#' {
-            cnt += 1;
-        }
-    }
-
-    if cnt == 2 && s1[0] == '#' && s2[1] == '#' || cnt == 2 && s1[1] == '#' && s2[0] == '#' {
-        println!("No");
-    } else {
-        println!("Yes");
+    match (s1.as_str(), s2.as_str()) {
+        ("#.", ".#") => println!("No"),
+        (".#", "#.") => println!("No"),
+        (_, _) => println!("Yes"),
     }
 }
