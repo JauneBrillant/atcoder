4ca8fa00c29d9f4a05ec8a025b27b8d6772f3b93
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 03:26:16 +0900

    another a

--- abc318/src/bin/a.rs
+++ abc318/src/bin/a.rs
@@ -1,17 +1,36 @@
-use proconio::input;
+// use proconio::input;
 
+// fn main() {
+//     input! {
+//         n: usize,
+//         mut m: usize,
+//         p: usize,
+//     }
+
+//     let mut res = 0;
+//     while m <= n {
+//         res += 1;
+//         m += p;
+//     }
+
+//     println!("{}", res);
+// }
+//
+
+use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut m: usize,
+        m: usize,
         p: usize,
     }
 
-    let mut res = 0;
-    while m <= n {
-        res += 1;
-        m += p;
+    if n < m {
+        println!("0");
+        return;
     }
 
+    let mut res = 1;
+    res += (n - m) / p;
     println!("{}", res);
 }
