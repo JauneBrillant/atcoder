2c80540257bc5a6767c8880b3e0889d79c36ddf7
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 03:16:26 +0900

    c

--- abc228/src/bin/c.rs
+++ abc228/src/bin/c.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        k: usize,
     }
 
+    let mut p = vec![];
+    for i in 0..n {
+        input! {
+            arr: [usize; 3],
+        }
+        let total = arr.iter().sum::<usize>();
+        p.push((i, total));
+    }
+
+    let mut sorted_p = p.clone();
+    sorted_p.sort_by(|(_, v1), (_, v2)| v2.cmp(v1));
+
+    for i in 0..n {
+        if p[i].1 + 300 >= sorted_p[k - 1].1 {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
