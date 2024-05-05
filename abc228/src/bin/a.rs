2353c2c4b115685ec87ba9bb3d967f7130aa0fa9
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 01:29:51 +0900

    a

--- abc228/src/bin/a.rs
+++ abc228/src/bin/a.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: usize,
+        t: usize,
+        x: usize,
     }
 
+    let mut times = vec![false; 24];
+    if s < t {
+        (s..t).for_each(|h| times[h] = true);
+    } else {
+        (s..24).for_each(|h| times[h] = true);
+        (0..t).for_each(|h| times[h] = true);
+    }
+
+    println!("{}", if times[x] { "Yes" } else { "No" });
 }
