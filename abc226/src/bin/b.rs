623a58e54f3dc5a03dca818af52408758d6d5754
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-05 21:02:14 +0900

    b

--- abc226/src/bin/b.rs
+++ abc226/src/bin/b.rs
@@ -1,8 +1,20 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
-
+        n: usize,
     }
 
+    let mut st = HashSet::new();
+
+    for _ in 0..n {
+        input! {
+            l: usize,
+            a: [usize; l],
+        }
+        st.insert(a);
+    }
+
+    println!("{}", st.len())
 }
