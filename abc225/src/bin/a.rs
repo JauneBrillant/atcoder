00c8cc0aba467adf0fe97417c4df91d43b9a65b4
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-05 18:02:13 +0900

    a

--- abc225/src/bin/a.rs
+++ abc225/src/bin/a.rs
@@ -1,8 +1,12 @@
+use itertools::Itertools;
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        s: Chars,
     }
 
+    let res = s.iter().permutations(3).unique().count();
+    println!("{}", res);
 }
