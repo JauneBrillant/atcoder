7aae9c30492eb1b21af34bbbb636494581d6adf5
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-05 22:47:31 +0900

    a

--- abc227/src/bin/a.rs
+++ abc227/src/bin/a.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        k: usize,
+        a: usize,
     }
 
+    let mut curr = a - 1;
+    for _ in 0..k {
+        curr += 1;
+        if curr == n + 1 {
+            curr = 1;
+        }
+    }
+
+    println!("{}", curr);
 }
