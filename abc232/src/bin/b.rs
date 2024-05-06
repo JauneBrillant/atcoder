22a018b2a327ca222805e02d0be2659223df03ba
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 19:12:51 +0900

    b

--- abc232/src/bin/b.rs
+++ abc232/src/bin/b.rs
@@ -1,8 +1,23 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        s: Chars,
+        t: Chars,
     }
 
+    for k in 0..26 {
+        let mut arr = vec![];
+        for &c in s.iter() {
+            let v = ((((c as u8 - 'a' as u8) as u8 + k) % 26) + 'a' as u8) as char;
+            arr.push(v);
+        }
+        if arr == t {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
