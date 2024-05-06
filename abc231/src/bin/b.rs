2f94f3996a2f958df5287bb951249fd88758d058
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 16:15:23 +0900

    b

--- abc231/src/bin/b.rs
+++ abc231/src/bin/b.rs
@@ -1,8 +1,17 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-
+        n: usize,
+        s: [String; n],
     }
 
+    let mut map = HashMap::new();
+    for str in s {
+        *map.entry(str).or_insert(0) += 1;
+    }
+
+    let res = map.iter().max_by_key(|&(_, &v)| v).unwrap();
+    println!("{}", res.0);
 }
