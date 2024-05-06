1560c932bf1de472d4cb695534c77b01733d5f09
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 15:48:33 +0900

    b

--- abc230/src/bin/b.rs
+++ abc230/src/bin/b.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let str = "oxxoxxoxxoxxoxxoxxoxxoxxoxxoxxoxxo";
+    for i in 0..3 {
+        if s == str[i..i + s.len()] {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
