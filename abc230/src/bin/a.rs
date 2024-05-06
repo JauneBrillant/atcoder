9a9c6cc51cae5a518963f3c137aed5d86bbf5370
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 15:22:35 +0900

    a

--- abc230/src/bin/a.rs
+++ abc230/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
     }
-
+    if n < 10 {
+        println!("AGC00{}", n);
+    } else if n < 42 {
+        println!("AGC0{}", n);
+    } else {
+        println!("AGC0{}", n + 1);
+    }
 }
