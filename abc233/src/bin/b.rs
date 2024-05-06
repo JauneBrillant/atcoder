15a5e93c19d299a7fc578c289ed438f722cb7a2b
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 19:47:50 +0900

    b

--- abc233/src/bin/b.rs
+++ abc233/src/bin/b.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        l: usize,
+        r: usize,
+        s: String,
     }
 
+    let head = &s[0..l - 1];
+    let mid = &s[l - 1..r].chars().rev().collect::<String>();
+    let tail = &s[r..];
+
+    let ss = format!("{}{}{}", head, mid, tail);
+    println!("{}", ss);
 }
