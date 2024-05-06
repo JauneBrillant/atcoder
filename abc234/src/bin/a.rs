2130c4b65085eb7be4bc90b446f290b5da5f9825
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 19:53:39 +0900

    a

--- abc234/src/bin/a.rs
+++ abc234/src/bin/a.rs
@@ -2,7 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        t: usize,
     }
+    let res = f(f(f(t) + t) + f(f(t)));
+    println!("{}", res);
+}
 
+fn f(x: usize) -> usize {
+    x * x + 2 * x + 3
 }
