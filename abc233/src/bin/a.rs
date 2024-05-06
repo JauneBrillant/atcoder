4447d18c835ec90d0402a57f426fb3e17dbb1d3e
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 19:25:25 +0900

    a

--- abc233/src/bin/a.rs
+++ abc233/src/bin/a.rs
@@ -2,7 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        x: usize,
+        y: usize,
     }
 
+    if x >= y {
+        println!("0");
+    } else {
+        let diff = y - x;
+        if diff % 10 == 0 {
+            println!("{}", diff / 10);
+        } else {
+            println!("{}", (diff / 10) + 1);
+        }
+    }
 }
