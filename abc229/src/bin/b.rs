68e10a819e2f405fa81047b1cd7a2983d7e83ab8
jaunebrillant <takahiro.hagiuda@gmail.com>
2024-05-06 14:00:11 +0900

    b

--- abc229/src/bin/b.rs
+++ abc229/src/bin/b.rs
@@ -2,7 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        mut a: usize,
+        mut b: usize,
     }
 
+    while a > 0 && b > 0 {
+        let a_digit = a % 10;
+        let b_digit = b % 10;
+        if a_digit + b_digit >= 10 {
+            println!("Hard");
+            return;
+        }
+        a /= 10;
+        b /= 10;
+    }
+
+    println!("Easy");
 }
