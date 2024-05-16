17e93985d0420ed7c551fe1837471db026dcac76
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-17 06:41:32 +0900

    b

--- abc236/src/bin/b.rs
+++ abc236/src/bin/b.rs
@@ -1,7 +1,21 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
+        n: usize,
+        a: [usize; (4 * n) - 1],
+    }
 
+    let mut map = HashMap::new();
+    for v in a {
+        *map.entry(v).or_insert(0) += 1;
+    }
+
+    for (k, v) in map {
+        if v == 3 {
+            println!("{}", k);
+            return;
+        }
     }
 }
