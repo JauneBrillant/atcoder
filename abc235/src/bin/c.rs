72bb19ed90e21ba93a375151dce1cd4ae1334ae1
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-16 12:28:37 +0900

    c

--- abc235/src/bin/c.rs
+++ abc235/src/bin/c.rs
@@ -1,8 +1,24 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
-
+        n: usize,
+        q: usize,
+        a: [usize; n],
+        xk: [(usize, usize); q],
     }
 
+    let mut hashmap = HashMap::new();
+    for i in 0..n {
+        hashmap.entry(a[i]).or_insert(vec![]).push(i + 1);
+    }
+
+    for &(x, k) in xk.iter() {
+        if hashmap.contains_key(&x) && hashmap[&x].len() >= k {
+            println!("{}", hashmap[&x][k - 1]);
+        } else {
+            println!("-1");
+        }
+    }
 }
