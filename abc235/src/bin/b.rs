fa517d6c6fb3989d2dc3f438929e431045f8361c
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-16 09:39:45 +0900

    b

--- abc235/src/bin/b.rs
+++ abc235/src/bin/b.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        h: [usize; n],
     }
 
+    let mut i = 0;
+    while i < n - 1 && h[i] < h[i + 1] {
+        i += 1;
+    }
+
+    println!("{}", h[i]);
 }
