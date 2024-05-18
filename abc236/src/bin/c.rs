fe0ac9cdd6b0fea47ea18842bc5860dca75024f9
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-18 11:50:24 +0900

    c

--- abc236/src/bin/c.rs
+++ abc236/src/bin/c.rs
@@ -2,6 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        s: [String; n],
+        t: [String; m],
+    }
 
+    let mut i = 0;
+    let mut j = 0;
+    while i < n && j < m {
+        if s[i] == t[j] {
+            println!("Yes");
+            i += 1;
+            j += 1;
+        } else {
+            println!("No");
+            i += 1;
+        }
     }
 }
