7d3561db62b557d65f3c2d12dc3f8a61fb2370be
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-18 12:01:19 +0900

    a

--- abc237/src/bin/a.rs
+++ abc237/src/bin/a.rs
@@ -2,6 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: i64,
+    }
 
+    if -2_i64.pow(31) <= n && n <= (2_i64.pow(31) - 1) {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
