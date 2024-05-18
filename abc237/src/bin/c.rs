f4750b2b413a85bd9c3fff1f319567fe3b5f3209
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-18 12:42:52 +0900

    c

--- abc237/src/bin/c.rs
+++ abc237/src/bin/c.rs
@@ -1,7 +1,31 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        s: Chars,
     }
+
+    let mut l = 0;
+    let mut r = s.len() - 1;
+    loop {
+        if l >= r {
+            println!("Yes");
+            return;
+        }
+
+        if s[l] == 'a' && s[r] == 'a' {
+            l += 1;
+            r -= 1;
+        } else if s[r] == 'a' {
+            r -= 1;
+        } else {
+            break;
+        }
+    }
+
+    let st = (l..=r).map(|i| s[i]).collect::<String>();
+    let rev_st = st.chars().rev().collect::<String>();
+
+    println!("{}", if st == rev_st { "Yes" } else { "No" });
 }
