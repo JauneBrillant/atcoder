23bcfbe1c7685fdb66a7188122d29051bc0fd88f
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-20 11:31:30 +0900

    b

--- abc354/src/bin/b.rs
+++ abc354/src/bin/b.rs
@@ -2,6 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        mut sc: [(String, usize); n],
     }
+
+    let sum = sc.iter().map(|(_, v)| v).sum::<usize>();
+    let t = sum % n;
+
+    sc.sort_by(|(n1, _), (n2, _)| n1.cmp(n2));
+    let res = &sc.iter().nth(t).unwrap().0;
+
+    println!("{}", res);
 }
