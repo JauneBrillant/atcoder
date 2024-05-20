1e131b25e46489f1545b323c7175727cbe857a17
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-20 14:00:38 +0900

    b

--- abc238/src/bin/b.rs
+++ abc238/src/bin/b.rs
@@ -2,6 +2,24 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
     }
+
+    let mut arr = vec![false; 360];
+    let mut curr = 0;
+    for ai in a {
+        curr += ai;
+        curr %= 360;
+        arr[curr] = true;
+    }
+
+    let mut max_len = 0;
+    let mut len = 0;
+    for v in arr {
+        if v { len = 1 } else { len += 1 }
+        max_len = max_len.max(len);
+    }
+
+    println!("{}", max_len);
 }
