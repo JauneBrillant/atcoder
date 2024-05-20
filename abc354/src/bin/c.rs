3828ae6d80b2c5bfc2f37b15b8047c605395225d
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-20 12:54:25 +0900

    c

--- abc354/src/bin/c.rs
+++ abc354/src/bin/c.rs
@@ -1,7 +1,42 @@
 use proconio::input;
 
+#[derive(Debug)]
+struct Card {
+    a: usize,
+    c: usize,
+    i: usize,
+}
+
 fn main() {
     input! {
+        n: usize,
+    }
+    
+    let mut acs = vec![];
+    for i in 0..n {
+        input! {
+            ai: usize,
+            ci: usize,
+        }
+        let aci = Card {a: ai, c: ci, i: i + 1};
+        acs.push(aci);
+    }
+    
+    acs.sort_by_key(|aci| aci.c);
 
+    let mut res = vec![];
+    let mut v = 0;
+    for card in acs.iter() {
+        if card.a > v {
+            v = card.a;
+            res.push(card.i);
+        }
+    }
+
+    res.sort();
+    println!("{}", res.len());
+    for v in res {
+        print!("{} ", v);
     }
 }
+
