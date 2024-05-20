fa582302fd691402956c6f4e08b27ce142072d67
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-20 11:31:05 +0900

    a

--- abc354/src/bin/a.rs
+++ abc354/src/bin/a.rs
@@ -2,6 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        h: i32,
     }
+
+    let mut i = 1;
+    while 2_i32.pow(i) - 1 <= h {
+        i += 1;
+    }
+
+    println!("{}", i);
 }
