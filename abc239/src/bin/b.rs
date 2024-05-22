2f75a24467cda0649e28e877578e2213591a4fc3
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-22 20:04:02 +0900

    b

--- abc239/src/bin/b.rs
+++ abc239/src/bin/b.rs
@@ -2,6 +2,12 @@ use proconio::input;
 
 fn main() {
     input! {
+        x: i64,
+    }
 
+    if x >= 0 {
+        println!("{}", x / 10);
+    } else {
+        println!("{}", -(-x + 9) / 10);
     }
 }
