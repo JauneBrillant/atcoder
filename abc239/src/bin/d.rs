8d6336598ed03f71a184456400c22e863bddca04
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-22 20:04:45 +0900

    d

--- abc239/src/bin/d.rs
+++ abc239/src/bin/d.rs
@@ -2,6 +2,32 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        a: usize,
+        b: usize,
+        c: usize,
+        d: usize,
     }
+
+    for i in a..=b {
+        if (c..=d).all(|j| !is_prime(i + j)) {
+            println!("Takahashi");
+            return;
+        }
+    }
+
+    println!("Aoki");
+}
+
+fn is_prime(x: usize) -> bool {
+    if x == 2 {
+        return true;
+    }
+
+    for i in 2..x {
+        if x % i == 0 {
+            return false;
+        }
+    }
+
+    true
 }
