82c5117fcf6373d5c8b63da481cee61619122f0e
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-23 13:11:41 +0900

    b

--- abc241/src/bin/b.rs
+++ abc241/src/bin/b.rs
@@ -2,6 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        b: [usize; m],
     }
+
+    for &bi in b.iter() {
+        if let Some(idx) = a.iter().position(|&ai| ai == bi) {
+            a.remove(idx);
+        } else {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
