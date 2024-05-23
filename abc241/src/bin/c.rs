ae7a276238b16a8180c35976faf0761555c5fefe
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-23 14:34:39 +0900

    c

--- abc241/src/bin/c.rs
+++ abc241/src/bin/c.rs
@@ -1,7 +1,38 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-
+        n: i32,
+        s: [Chars; n],
     }
+
+    let directions = [(0, 1), (1, 0), (1, 1), (-1, 1)];
+
+    for i in 0..n {
+        for j in 0..n {
+            'outer: for (di, dj) in directions {
+                let mut ni = i;
+                let mut nj = j;
+                let mut cnt = 0;
+
+                for _ in 0..6 {
+                    if ni < 0 || nj < 0 || ni >= n || nj >= n {
+                        continue 'outer;
+                    }
+                    if s[ni as usize][nj as usize] == '.' {
+                        cnt += 1;
+                    }
+                    ni += di;
+                    nj += dj;
+                }
+                if cnt <= 2 {
+                    println!("Yes");
+                    return;
+                }
+            }
+        }
+    }
+
+    println!("No");
 }
