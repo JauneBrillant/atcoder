17783fd63adaa12e0a38e588e094b61c68e0391f
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-24 11:32:44 +0900

    c

--- abc342/src/bin/c.rs
+++ abc342/src/bin/c.rs
@@ -1,8 +1,26 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+use std::collections::HashMap;
 
 fn main() {
     input! {
-
+        n: usize,
+        mut s: Chars,
+        q: usize,
+        cd: [(char, char); q],
     }
 
+    let mut az = ('a'..='z').collect::<Vec<char>>();
+    for (c, d) in cd {
+        for i in 0..26 {
+            if az[i] == c {
+                az[i] = d;
+            }
+        }
+    }
+
+    for i in 0..n {
+        s[i] = az[s[i] as usize - 'a' as usize];
+    }
+
+    println!("{}", s.iter().collect::<String>());
 }
