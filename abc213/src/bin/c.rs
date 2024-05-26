c090747439440bf5f251c94d021db249c8f5c56a
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-26 20:14:25 +0900

    c

--- abc213/src/bin/c.rs
+++ abc213/src/bin/c.rs
@@ -1,8 +1,21 @@
 use proconio::input;
+use std::collections::{BTreeSet, HashMap};
 
 fn main() {
     input! {
-
+        _h: usize,
+        _w: usize,
+        n: usize,
+        ab: [(usize, usize); n],
     }
 
+    let rows: BTreeSet<_> = ab.iter().map(|&(a, _)| a).collect();
+    let cols: BTreeSet<_> = ab.iter().map(|&(_, b)| b).collect();
+
+    let rows_map: HashMap<_, _> = rows.iter().enumerate().map(|(i, &r)| (r, i + 1)).collect();
+    let cols_map: HashMap<_, _> = cols.iter().enumerate().map(|(i, &c)| (c, i + 1)).collect();
+
+    for (a, b) in &ab {
+        println!("{} {}", rows_map[a], cols_map[b]);
+    }
 }
