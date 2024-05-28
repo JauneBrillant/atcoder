fe85a7644076b504b9f478a72cba31f3ecf983d1
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-28 17:32:11 +0900

    d

--- abc328/src/bin/d.rs
+++ abc328/src/bin/d.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
     }
 
+    let res = s
+        .chars()
+        .fold(vec![], |mut stack, c| {
+            stack.push(c);
+            let len = stack.len() - 1;
+            if stack.len() >= 3
+                && stack[len] == 'C'
+                && stack[len - 1] == 'B'
+                && stack[len - 2] == 'A'
+            {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+            }
+            stack
+        })
+        .iter()
+        .collect::<String>();
+
+    println!("{}", res);
 }
