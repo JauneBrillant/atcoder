378f410ab0f53c80f6c50d4a07dca16bbfac29e4
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-05-31 19:42:04 +0900

    004

--- typical-90/src/bin/004.rs
+++ typical-90/src/bin/004.rs
@@ -7,14 +7,8 @@ fn main() {
         a: [[usize; w]; h],
     }
 
-    let mut rows = vec![0; h];
-    let mut cols = vec![0; w];
-    for i in 0..h {
-        for j in 0..w {
-            rows[i] += a[i][j];
-            cols[j] += a[i][j];
-        }
-    }
+    let rows: Vec<usize> = a.iter().map(|vec| vec.iter().sum()).collect();
+    let cols: Vec<usize> = (0..w).map(|j| a.iter().map(|row| row[j]).sum()).collect();
 
     for i in 0..h {
         for j in 0..w {
