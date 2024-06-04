a387d0a1c4a4a3f7b8c0dd9ec8100dde957970da
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-06-04 22:51:11 +0900

    refactor: c

--- ABC/abc356/src/bin/c.rs
+++ ABC/abc356/src/bin/c.rs
@@ -1,43 +1,33 @@
+use itertools::Itertools;
 use proconio::input;
-
-struct Question {
-    c: usize,
-    a: Vec<usize>,
-    r: char,
-}
+use proconio::marker::Usize1;
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        k: usize,
+        k: u32,
     }
 
-    let mut aa = vec![];
-    for _ in 0..m {
-        input! {
-            c: usize,
-            a: [usize; c],
-            r: char,
-        }
-        aa.push(Question { c: c, a: a, r: r });
-    }
-
-    let mut res = 0;
-    'outer: for bit in 0..(1 << n) {
-        for q in aa.iter() {
-            let mut correct_key_count = 0;
-            for &key in q.a.iter() {
-                if (bit & (1 << (key - 1))) != 0 {
-                    correct_key_count += 1;
-                }
+    let trials = (0..m)
+        .map(|_| {
+            input! {
+                c: usize,
+                a: [Usize1; c],
+                r: char,
             }
-            if (q.r == 'o' && correct_key_count < k) || (q.r == 'x' && correct_key_count >= k) {
-                continue 'outer;
-            }
-        }
-        res += 1;
-    }
+            let a = a.iter().fold(0, |acc, &x| acc | 1 << x);
+            (a, r == 'o')
+        })
+        .collect_vec();
 
-    println!("{}", res);
+    let ans = (0u32..(1 << n))
+        .filter(|bs| {
+            trials
+                .iter()
+                .all(|&(a, r)| r == ((a & bs).count_ones() >= k))
+        })
+        .count();
+
+    println!("{}", ans);
 }
