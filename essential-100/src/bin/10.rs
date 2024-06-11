5c3135f51733e7ae066a52f99969d32c45884213
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-06-11 10:57:37 +0900

    10

--- essential-100/src/bin/10.rs
+++ essential-100/src/bin/10.rs
@@ -1,1 +1,32 @@
+use proconio::input;
 
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n],
+        q: usize,
+        m: [usize; q],
+    }
+
+    'outer: for &mi in m.iter() {
+        for bit in 0..(1 << n) {
+            let bs = format!("{:b}", bit);
+            let sums =
+                bs.chars().rev().enumerate().fold(
+                    0,
+                    |acc, (i, e)| {
+                        if e == '1' {
+                            acc + a[i]
+                        } else {
+                            acc
+                        }
+                    },
+                );
+            if sums == mi {
+                println!("Yes");
+                continue 'outer;
+            }
+        }
+        println!("No");
+    }
+}
