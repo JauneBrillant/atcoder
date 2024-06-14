5918cf33d0974dcb869d61b225f47506a828fff2
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-06-14 21:44:19 +0900

    add: another solution

--- tessoku/python/A13.py
+++ tessoku/python/A13.py
@@ -1,4 +1,19 @@
+# import bisect
+
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
-left, right = 0, n
+# ans = 0
+# for i, e in enumerate(a):
+#     ans += bisect.bisect(a, e + k) - i - 1
+
+# print(ans)
+
+ans = 0
+r = 0
+for l in range(n):
+    while r < n and a[r] - a[l] <= k:
+        r += 1
+    ans += r - l - 1
+
+print(ans)
