86ddbf6eb9626bce949f13355cb073fd28aa4400
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-07-01 08:03:46 +0900

    refactor: b

--- ABC/abc360/b.py
+++ ABC/abc360/b.py
@@ -1,9 +1,9 @@
 s, t = input().split()
 
-ok = False
 for w in range(1, len(s)):
     for c in range(w):
         if s[c::w] == t:
-            ok = True
+            print("Yes")
+            exit()
 
-print("Yes" if ok else "No")
+print("No")
