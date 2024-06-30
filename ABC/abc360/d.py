11f18a71549e06e1dbba25c6d386c94b4ec59664
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-07-01 06:12:43 +0900

    d

--- ABC/abc360/d.py
+++ ABC/abc360/d.py
@@ -8,15 +8,16 @@ pos_ants = []
 neg_ants = []
 for i in range(n):
     if s[i] == "1":
-        pos_ants.append(x[i] + t)
+        pos_ants.append(x[i])
     else:
-        neg_ants.append(x[i] - t)
+        neg_ants.append(x[i])
 
+pos_ants.sort()
 neg_ants.sort()
 
 ans = 0
-for pos in pos_ants:
-    idx = bisect.bisect(x, pos - t)
-    ans += len(neg_ants) - idx
+for p in pos_ants:
+    r = p + t * 2
+    ans += bisect.bisect(neg_ants, r) - bisect.bisect(neg_ants, p)
 
 print(ans)
