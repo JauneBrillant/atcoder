aba845e7f7378613a6f824f35e2e3ee3a45c5bbc
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-07-02 08:19:52 +0900

    c

--- ABC/abc203/c.py
+++ ABC/abc203/c.py
@@ -2,17 +2,11 @@ n, k = map(int, input().split())
 ab = [tuple(map(int, input().split())) for _ in range(n)]
 
 ab.sort()
-
-deposit = k
-pos = 0
-prev = 0
+curr = k
 for a, b in ab:
-    if deposit - (a - pos) < 0:
+    if curr >= a:
+        curr += b
+    else:
         break
-    if prev != a:
-        deposit -= a - pos
-    pos = a
-    deposit += b
-    prev = a
 
-print(pos + deposit)
+print(curr)
