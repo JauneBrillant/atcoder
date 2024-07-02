82cd04ab878b429b5d829d9a31a9590410485870
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-07-03 04:48:20 +0900

    c

--- ABC/abc206/c.py
+++ ABC/abc206/c.py
@@ -1,22 +1,14 @@
-n = int(input())
+from collections import defaultdict
+from typing import DefaultDict
 
-intervals = []
-for _ in range(n):
-    t, l, r = map(float, input().split())
-    match t:
-        case 1:
-            intervals.append((l, r))
-        case 2:
-            intervals.append((l, r - 0.1))
-        case 3:
-            intervals.append((l - 0.1, r))
-        case 4:
-            intervals.append((l - 0.1, r - 0.1))
+n = int(input())
+a = list(map(int, input().split()))
+
+hashmap: DefaultDict[int, int] = defaultdict(int)
 
 ans = 0
 for i in range(n):
-    for j in range(i + 1, n):
-        if intervals[i][r] >= intervals[j][l]:
-            ans += 1
+    ans += i - hashmap[a[i]]
+    hashmap[a[i]] += 1
 
 print(ans)
