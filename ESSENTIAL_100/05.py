80dd9c851271f39b1827b1a3eeddfc5baae809d4
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-01 08:10:19 +0900

    1

--- ESSENTIAL_100/05.py
+++ ESSENTIAL_100/05.py
@@ -1,4 +1,12 @@
-a, b, c, x, y = map(int, input().split())
+A, B, C, X, Y = map(int, input().split())
+ans = 10**9
 
-for i in range(1, x + 1):
-    for j in range(1, y + 1):
+ab = [i for i in range(max(X, Y) * 2 + 9) if i % 2 == 0]
+for k in ab:
+    i = X - k // 2
+    j = Y - k // 2
+    if i < 0 or j < 0:
+        continue
+    ans = min(ans, A * i + B * j + C * k)
+
+print(ans)
