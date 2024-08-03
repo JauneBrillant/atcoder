c7f0e4098441058ea89617d58cb3af37cd327628
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-03 14:30:13 +0900

    2

--- abc/abc277/c.py
+++ abc/abc277/c.py
@@ -1,25 +1,29 @@
 import sys
 
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10**6)
+
 from collections import defaultdict
 
 n = int(input())
 graph = defaultdict(list)
 for _ in range(n):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    u, v = map(int, input().split())
+    u -= 1
+    v -= 1
+    graph[u].append(v)
+    graph[v].append(u)
+ans = 0
 
 
-def dfs(curr_v, visited):
-    visited.add(curr_v)
-    highest = curr_v
-    for v in graph[curr_v]:
-        if v in visited:
-            continue
-        highest = max(highest, dfs(v, visited))
-    return highest
+def dfs(v, visited):
+    global ans
+    ans = max(ans, v)
+    visited.add(v)
+    for neighbor in graph[v]:
+        if neighbor not in visited:
+            dfs(neighbor, visited)
 
 
 visited = set()
-print(dfs(1, visited))
+dfs(0, visited)
+print(ans + 1)
