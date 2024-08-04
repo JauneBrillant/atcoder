bf3292f0a7a38dd140dfdf7c8002bd42236fbd38
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-04 13:08:54 +0900

    resolve: c

--- abc/abc365/c.py
+++ abc/abc365/c.py
@@ -1,21 +1,26 @@
-n, m = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort()
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
 
-
-def is_ok(x):
-    total = sum(min(x, ai) for ai in a)
-    return total <= m
-
-
-ok, ng = -1, m + 1
-while ng - ok > 1:
-    mid = (ok + ng) // 2
-    if is_ok(mid):
-        ok = mid
-    else:
-        ng = mid
-if ok == m:
+# 交通費支給額の上限を儲けることはしなくても（全員に交通費を満額支給しても）予算以内の場合（M以下の場合）
+if sum(A) <= M:
     print("infinite")
-else:
-    print(ok)
+    exit()
+
+
+def cond(x):
+    total_cost = sum(min(a, x) for a in A)
+    return total_cost <= M
+
+
+# 交通費支給額の上限の範囲を二分探索して、トータルの出費が予算以下(M)になる範囲を調べる
+# ok : 交通費支給額上限が0円だと、一円もコストがかからないので確実に条件を満たす (total_cost <= M)
+# ng : 交通費支給額上限を一番交通費がかかる人にすると(全員に交通費をを満額支給)、予算(M)をオーバーするのでNG (sum(A) <= M でないことを既に確認済み)
+ok, ng = 0, max(A)
+while ok + 1 < ng:
+    mi = (ok + ng) // 2
+    if cond(mi):
+        ok = mi
+    else:
+        ng = mi
+
+print(ok)
