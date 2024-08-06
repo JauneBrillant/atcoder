da9586aa28ea0514d13905b1a8809c8e44eb2cd0
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-06 13:57:10 +0900

    2

--- edpc/a.py
+++ edpc/a.py
@@ -1,11 +1,25 @@
 n = int(input())
 h = list(map(int, input().split()))
 
-dp = [0] * n
-dp[1] = abs(h[0] - h[1])
-for i in range(2, n):
-    one_step = dp[i - 1] + abs(h[i - 1] - h[i])
-    two_step = dp[i - 2] + abs(h[i - 2] - h[i])
-    dp[i] = min(one_step, two_step)
+# 貰うDP
+
+# dp = [0] * n
+# dp[1] = abs(h[0] - h[1])
+# for i in range(2, n):
+#     one_step = dp[i - 1] + abs(h[i - 1] - h[i])
+#     two_step = dp[i - 2] + abs(h[i - 2] - h[i])
+#     dp[i] = min(one_step, two_step)
+
+# print(dp[n - 1])
+
+
+# 配るDP
+
+dp = [float("inf")] * n
+dp[0] = 0
+for i in range(n - 1):
+    dp[i + 1] = min(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]))
+    if i + 2 < n:
+        dp[i + 2] = min(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]))
 
 print(dp[n - 1])
