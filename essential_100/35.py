f78096228b6211a5aba55a7f83099e8b8bd4ed5a
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-07 11:28:08 +0900

    2

--- essential_100/35.py
+++ essential_100/35.py
@@ -1,21 +1,19 @@
-n, w = map(int, input().split())
-values = [0]
-weights = [0]
-for _ in range(n):
-    a, b = map(int, input().split())
-    values.append(a)
-    weights.append(b)
+N, M = map(int, input().split())
+V = [0]
+W = [0]
+for _ in range(N):
+    v, w = map(int, input().split())
+    V.append(v)
+    W.append(w)
 
-# dp[i][j]
-# i番目までの商品から重さの総和がj以下になるように選んだ時の、価値の和の最大値
+# dp[i][j] := 品物iまで選んだ時の重さ制限jまででの最大価値
+dp = [[0] * (M + 1) for _ in range(N + 1)]
 
-dp = [[0] * (w + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    for j in range(w + 1):
-        if j < weights[i]:
+for i in range(1, N + 1):
+    for j in range(M + 1):
+        if j < W[i]:
             dp[i][j] = dp[i - 1][j]
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i])
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i])
 
-print(dp[n][w])
+print(dp[N][M])
