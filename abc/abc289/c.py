26ea63db80161d0f0851033cc2707e9992475331
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-08 18:56:35 +0900

    2

--- abc/abc289/c.py
+++ abc/abc289/c.py
@@ -1,19 +1,19 @@
 N, M = map(int, input().split())
 C = []
 for _ in range(M):
-    size = int(input())
-    a = list(map(int, input().split()))
+    _ = int(input())
+    a = set(map(int, input().split()))
     C.append(a)
-
-x = [i for i in range(1, N + 1)]
+correct = set([x for x in range(1, N + 1)])
+    
 ans = 0
 for bit in range(1 << M):
     st = set()
     for i in range(M):
         if bit & (1 << i):
-            for e in C[i]:
-                st.add(e)
-    if sorted(st) == x:
+            st |= C[i]
+    
+    if st == correct:
         ans += 1
 
 print(ans)
