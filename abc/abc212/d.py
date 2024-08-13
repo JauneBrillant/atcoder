7d7e15b3bd83a662a3182870a8ce65d8fd53a66e
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-14 00:10:54 +0900

    2

--- abc/abc212/d.py
+++ abc/abc212/d.py
@@ -1,23 +1,43 @@
-import heapq
+# import heapq
+
+# Q = int(input())
+
+# heap = []
+# res = []
+# add = 0
+
+# for _ in range(Q):
+#     query = list(map(int, input().split()))
+#     type = query[0]
+
+#     match type:
+#         case 1:
+#             x = query[1]
+#             heapq.heappush(heap, x - add)
+#         case 2:
+#             x = query[1]
+#             add += x
+#         case 3:
+#             res.append(heapq.heappop(heap) + add)
+
+# print(*res)
+
+from sortedcontainers import SortedList
 
 Q = int(input())
 
-heap = []
-res = []
 add = 0
+sl = SortedList()
 
 for _ in range(Q):
     query = list(map(int, input().split()))
     type = query[0]
-
     match type:
         case 1:
             x = query[1]
-            heapq.heappush(heap, x - add)
+            sl.add(x - add)
         case 2:
             x = query[1]
             add += x
         case 3:
-            res.append(heapq.heappop(heap) + add)
-
-print(*res)
+            print(sl.pop(0) + add)
