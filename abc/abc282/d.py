a64882a50caf7b18589c09f61a70a3d756aa6459
takahiro.hagiuda <takahiro.hagiuda@gmail.com>
2024-08-18 20:25:23 +0900

    1

--- abc/abc282/d.py
+++ abc/abc282/d.py
@@ -1,48 +1,42 @@
-import sys
-
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 N, M = map(int, input().split())
+E = list(tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(M))
 G = [[] for _ in range(N)]
-for _ in range(M):
-    u, v = map(int, input().split())
-    u -= 1
-    v -= 1
+for u, v in E:
     G[u].append(v)
     G[v].append(u)
 
 
-def solve(v, c):
-    def dfs(v, c):
-        nonlocal a, b, edge, is_bipartite
-        colors[v] = c
-        if c:
-            a += 1
-        else:
-            b += 1
-
-        for nv in G[v]:
-            edge += 1
-            if colors[nv] == -1:
-                dfs(nv, not c)
-            elif colors[nv] == c:
-                is_bipartite = False
-
-    a, b, edge = 0, 0, 0
-    is_bipartite = True
-
-    dfs(v, c)
-
-    if is_bipartite:
-        return a * b - edge // 2
-    else:
-        return 0
+def com(n):
+    return n * (n - 1) // 2
 
 
-ans = 0
+ng_edges = 0
+is_bipartite = True
 colors = [-1] * N
 for i in range(N):
-    if colors[i] == -1:
-        ans += solve(i, 0)
+    if colors[i] != -1:
+        continue
 
-print(ans)
+    white_v_cnt, black_v_cnt = 0, 0
+    q = deque([(i, 0)])
+    colors[i] = 0
+
+    while q:
+        u, c = q.popleft()
+        if c == 0:
+            white_v_cnt += 1
+        else:
+            black_v_cnt += 1
+
+        for v in G[u]:
+            if colors[v] == c:
+                is_bipartite = False
+            if colors[v] == -1:
+                colors[v] = not c
+                q.append((v, not c))
+
+    ng_edges += com(white_v_cnt) + com(black_v_cnt)
+
+print(com(N) - M - ng_edges if is_bipartite else 0)
